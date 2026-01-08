160827bb8796e3ce0f6dbd77e7b034a019af02cf
Sthefany Rocha <sthefanyrrocha7@gmail.com>
2026-01-08 01:10:58 -0300

    Add menu for task operations in Menu.java

---
+++ to-do-list/Menu.java
@@ -0,0 +1,25 @@
+package todolist;
+
+import java.util.Scanner;
+
+public class Operacoes_Menu {
+    Scanner sc = new Scanner(System.in);
+    int opcao = -1;
+
+    public int menu(){
+        System.out.println("");
+        System.out.println("        Menu de Opções:        ");
+        System.out.println("1) Adicionar Nova Atividade");
+        System.out.println("2) Pesquisar Atividade por Título");
+        System.out.println("3) Excluir Atividade pelo Título");
+        System.out.println("4) Atualizar Atividade pelo Título");
+        System.out.println("5) Iniciar Atividade");
+        System.out.println("6) Finalizar Atividade");
+        System.out.println("7) Listar Atividades (com Submenu)");
+        System.out.println("8) Sair do Sistema");
+        System.out.println("Escolha uma opção: ");
+        opcao = sc.nextInt();
+        sc.nextLine();
+        return opcao;
+    }
+}
